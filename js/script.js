3aecdd10a2f286ccc605f9e8e81b4fb666aeaa56
rplbotv26 <162473357+rplbotv26@users.noreply.github.com>
2024-09-11 10:11:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-	// Sticky Navbar
-    let header = document.querySelector('header');
-    let menu = document.querySelector('#menu-icon');
-    let navbar = document.querySelector('.navbar');
-     
-     
-    window.addEventListener('scroll', () => {
-        header.classList.toggle('shadow', window.scrollY > 0);
-    });
-     
-    menu.onclick = () => {
-        navbar.classList.toggle('active');
-    }
-    window.onscroll = () => {
-        navbar.classList.remove('active');
-    }
-     
-    // Dark Mode
-    let darkmode = document.querySelector('#darkmode');
-     
-    darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
-            document.body.classList.add('active');
-        }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
-            document.body.classList.remove('active');
-        }
-    }
+// Sticky Navbar
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+// Form Submission
+function submitForm() {
+  // Mendapatkan elemen-elemen formulir
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Validasi input (Opsional)
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill out all fields.");
+    return;
+  }
+
+  // Menampilkan pesan pengiriman (Opsional)
+  alert("Message sent!");
+
+  // Kosongkan semua field formulir
+  document.getElementById("contactForm").reset();
+}
